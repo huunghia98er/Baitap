a8b9147f57b26c46b09c6a24bf95497583872a68
huunghia98er <huunghia98er@gmail.com>
2023-05-06 18:37:26 +0700

    a

--- addSV/sv.js
+++ addSV/sv.js
@@ -1,14 +1,9 @@
-const array =[{
-    'key1': '10A1',
-    'key2': '10A2',
-    'key3': '10A3',
-    'key4': '11A1',
-    'key5': '11A2',
-    'key6': '11A3',
-    'key7': '12A1',
-    'key8': '12A2',
-    'key9': '12A3',
-}];
+const array =[
+    {'class1': 'Aptech'},
+    {'class2': 'Cao dang FPT'},
+    {'class3': 'Dai hoc FPT'},
+    {'class4': 'FUNiX'}
+];
 let Students_List = `<tr>
     <th>ID</th>
     <th>Họ và tên</th>
@@ -38,7 +33,7 @@ submit.addEventListener('click', function(){
         Class: Class_Input.value,
         ID: ID_Input.value
     }
-
+    // check_student(Student, Students);
     Students.push(Student);
 
     Students_List += `<tr>
@@ -73,45 +68,6 @@ function tolocalstorage(){
     }
 }
 
-    submit.addEventListener('click', function(){
-        let Name_Input = document.getElementById('name')
-        let ID_Input = document.getElementById('IDsv')
-        let Class_Input = document.getElementById('Class')
-        let Age_Input = document.getElementById('age')
-        let Student = {
-            Name: Name_Input.value,
-            Age: Age_Input.value,
-            Class: Class_Input.value,
-            ID: ID_Input.value
-        }
-        Students.push(Student);
-        Students_List += `<tr>
-                <td>${Student.ID}</td>
-                <td>${Student.Name}</td>
-                <td>${Student.Age}</td>
-                <td>${Student.Class}</td>
-            </tr>`
-        Name_Input.value = ''
-        ID_Input.value = ''
-        Class_Input.value = ''
-        Age_Input.value = ''
-        tablelist.innerHTML = Students_List
-        std_list.style.display = 'block'
-        localStorage.setItem('Students', JSON.stringify(Students))
-    })
-
-    function tolocalstorage(){
-        Students = (localStorage.getItem('Students') !== null) ? JSON.parse(localStorage.getItem('Students')) : []
-        if (Students.length > 0){
-            Students.forEach((Students) => {
-                Students_List += `<tr>
-                <td>${Students.ID}</td>
-                <td>${Students.Name}</td>
-                <td>${Students.Age}</td>
-                <td>${Students.Class}</td>
-            </tr>`
-            })
-            tablelist.innerHTML = Students_List
-            std_list.style.display = 'block'
-        }
-    }
+// function check_student(stu, stus) {
+//     forEach()
+// }
